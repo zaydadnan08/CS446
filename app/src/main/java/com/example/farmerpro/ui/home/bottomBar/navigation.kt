7e9971a85468fc4f15dae9afbe204f167305b586
David Mehic <davidmehic5@gmail.com>
2023-06-25 17:51:33 -0400

    home pages + routes

--- app/src/main/java/com/example/farmerpro/ui/home/bottomBar/navigation.kt
+++ app/src/main/java/com/example/farmerpro/ui/home/bottomBar/navigation.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.farmerpro.ui.home.farmer.FarmScreen
+import com.example.farmerpro.ui.home.fridge.CommunityFridgeScreen
+import com.example.farmerpro.ui.home.market.MarketScreen
 import com.example.farmerpro.ui.landing.StartScreen
 import com.example.farmerpro.ui.landing.UserSelectScreen
 import com.example.farmerpro.ui.landing.UserSignupScreen
@@ -13,13 +16,13 @@ import com.example.farmerpro.ui.landing.UserSignupScreen
 fun navigation(navController: NavHostController){
     NavHost(navController = navController, startDestination = Screens.Home.name){
         composable(route = Screens.Home.name) {
-            StartScreen(navController = navController)
+            MarketScreen(navController = navController)
         }
         composable(route = Screens.Farmer.name) {
-            UserSelectScreen(navController = navController)
+            FarmScreen(navController = navController)
         }
         composable(route = Screens.Fridge.name) {
-            UserSignupScreen(navController = navController)
+            CommunityFridgeScreen(navController = navController)
         }
     }
 }
---
+++ app/src/main/java/com/example/farmerpro/ui/home/fridge/communityfridgescreen.kt
@@ -0,0 +1,35 @@
+package com.example.farmerpro.ui.home.fridge
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.farmerpro.Screens
+
+@Composable
+fun CommunityFridgeScreen(navController: NavController){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            navController.navigate(Screens.UserSelect.name);
+        },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "click here for Community Fridge Screen")
+        }
+    }
+}
