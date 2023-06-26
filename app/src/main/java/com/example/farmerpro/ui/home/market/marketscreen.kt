6f1584ad7575c5522c90474472d3512b8da299bc
Zayd Adnan <zaydadnan@v1040-wn-rt-a-10-37.campus-dynamic.uwaterloo.ca>
2023-06-25 21:19:16 -0400

    merge conflicts resolved

--- app/src/main/java/com/example/farmerpro/ui/home/market/marketscreen.kt
+++ app/src/main/java/com/example/farmerpro/ui/home/market/marketscreen.kt
@@ -3,21 +3,15 @@ package com.example.farmerpro.ui.home.market
 import Item
 import MarketplaceItemList
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.farmerpro.Screens
-import com.example.farmerpro.ui.home.fridge.fridge
 
 @Preview
 @Composable
