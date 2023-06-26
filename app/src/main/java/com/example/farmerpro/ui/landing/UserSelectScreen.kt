4f2cb8ecfe4c440142fade641d1afa3a7de2ea4d
David Mehic <davidmehic5@gmail.com>
2023-06-26 02:33:43 -0400

    button rounding

--- app/src/main/java/com/example/farmerpro/ui/landing/UserSelectScreen.kt
+++ app/src/main/java/com/example/farmerpro/ui/landing/UserSelectScreen.kt
@@ -1,12 +1,15 @@
 package com.example.farmerpro.ui.landing
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,22 +39,28 @@ fun UserSelectScreen(navController: NavController){
             onClick = {
                 navController.navigate(Screens.UserSignup.name);
             },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
         ) {
-            Text(text = "customer")
+            Text(text = "customer", color = Color.Black)
         }
         Button(
             onClick = {
                 navController.navigate(Screens.UserSignup.name);
             },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
         ) {
-            Text(text = "farmer")
+            Text(text = "farmer", color = Color.Black)
         }
         Button(
             onClick = {
                 navController.navigate(Screens.UserSignup.name);
             },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
         ) {
-            Text(text = "community fridge")
+            Text(text = "community fridge", color = Color.Black)
         }
     }
 }
--- app/src/main/java/com/example/farmerpro/ui/landing/UserSignupScreen.kt
+++ app/src/main/java/com/example/farmerpro/ui/landing/UserSignupScreen.kt
@@ -2,11 +2,14 @@ package com.example.farmerpro.ui.landing
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -62,8 +65,10 @@ fun UserSignupScreen(navController: NavController) {
             onClick = {
                 navController.navigate(Screens.Home.name);
             },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
         ) {
-            Text(text = "Sign Up")
+            Text(text = "Sign Up", color = Color.Black)
         }
     }
 }
