3ef974b96f7f90dc9edfc2d2b062bf77c49f150f
Shehryar Suleman <shehryarsuleman@gmail.com>
2023-07-19 02:31:09 -0400

    add track sale popup

---
+++ app/src/main/java/com/example/farmerpro/ui/home/farmer/components/TrackSaleDialog.kt
@@ -0,0 +1,79 @@
+package com.example.farmerpro.ui.home.farmer.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.farmerpro.components.GreyTextInput
+import kotlinx.coroutines.job
+
+@Composable
+fun TrackSaleDialog(
+    closeDialog: () -> Unit
+) {
+    var quantity by remember { mutableStateOf("") }
+
+    var price by remember { mutableStateOf("") }
+
+    val focusRequester = FocusRequester()
+
+    AlertDialog(
+        onDismissRequest = closeDialog,
+        text = {
+            Column {
+                Text(
+                    text = "Track Sale",
+                    modifier = Modifier.padding(4.dp), style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Start,
+                        color = Color.Black
+                    )
+                )
+
+                GreyTextInput(value = quantity, onValueChange = { quantity = it } , placeholder = "Quantity Sold", Modifier.focusRequester(focusRequester))
+                LaunchedEffect(Unit) {
+                    coroutineContext.job.invokeOnCompletion {
+                        focusRequester.requestFocus()
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                GreyTextInput(value = price, onValueChange =  { price = it } , placeholder = "Price")
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    closeDialog()
+                }
+            ) {
+                Text(
+                    text = "Save"
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = closeDialog
+            ) {
+                Text(
+                    text = "Dismiss"
+                )
+            }
+        }
+    )
+}
