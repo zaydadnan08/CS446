8dbad0f9c795d8a3b40ad02374971ef17817e06e
Shehryar Suleman <shehryarsuleman@gmail.com>
2023-07-25 18:22:53 -0400

    add voice to text inventory changes

--- app/src/main/java/com/example/farmerpro/ui/home/farmer/components/ItemRow.kt
+++ app/src/main/java/com/example/farmerpro/ui/home/farmer/components/ItemRow.kt
@@ -1,5 +1,7 @@
 package com.example.farmerpro.ui.home.farmer.components
 
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -26,6 +28,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,18 +36,39 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.farmerpro.R
 import com.example.farmerpro.domain.model.InventoryItem
 import com.example.farmerpro.domain.model.MarketplaceItem
+import com.example.farmerpro.ui.SpeechRecognizerContract
 import com.example.farmerpro.ui.home.farmer.farmViewModel
 import com.example.farmerpro.ui.theme.Gray400
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalPermissionsApi::class)
 @Composable
 fun ItemRow(
     item: InventoryItem,
     viewModel: farmViewModel = hiltViewModel(),
     onClick: () -> Unit
 ) {
+    val permissionState = rememberPermissionState(
+        permission = Manifest.permission.RECORD_AUDIO
+    )
+    SideEffect {
+        permissionState.launchPermissionRequest()
+    }
+
+    val speechRecognizerLauncher = rememberLauncherForActivityResult(
+        contract = SpeechRecognizerContract(),
+        onResult = {
+            if (it != null) {
+                if (it.isNotEmpty() && it[0].toDoubleOrNull() != null)
+                    viewModel.updateInventoryItem(item.name, item.quantity + it?.get(0)?.toDouble()!!, item.unit, item.notes)
+            }
+        }
+    )
     Card(
         modifier = Modifier
             .padding(8.dp)
@@ -90,9 +114,12 @@ fun ItemRow(
                         tint = LocalContentColor.current
                     )
                 }
-                IconButton(onClick = { viewModel.deleteItem(item.name)}) {
+                IconButton(onClick = { if (permissionState.status.isGranted) {
+                    speechRecognizerLauncher.launch(Unit)
+                } else
+                    permissionState.launchPermissionRequest()}) {
                     Icon(
-                        Icons.Filled.Delete,
+                        painter = painterResource(id = R.drawable.baseline_mic_24) ,
                         contentDescription = "Delete",
                         tint = LocalContentColor.current
                     )
