3959df585d962118a8b8ee9072090cb5e6744991
Hamza Saqib <chaosemerald890@gmail.com>
2023-07-26 01:36:11 -0400

    filtering and scolling

--- app/src/main/java/com/example/farmerpro/ui/home/farmer/components/SalesRow.kt
+++ app/src/main/java/com/example/farmerpro/ui/home/farmer/components/SalesRow.kt
@@ -1,40 +1,22 @@
 package com.example.farmerpro.ui.home.farmer.components
 
-import android.Manifest
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,23 +24,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.farmerpro.R
-import com.example.farmerpro.domain.model.InventoryItem
-import com.example.farmerpro.domain.model.MarketplaceItem
 import com.example.farmerpro.domain.model.SaleRecord
-import com.example.farmerpro.ui.SpeechRecognizerContract
 import com.example.farmerpro.ui.home.farmer.farmViewModel
 import com.example.farmerpro.ui.theme.Gray400
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalPermissionsApi::class)
 @Composable
@@ -75,7 +47,8 @@ fun SalesRow(
             .border(
                 BorderStroke(0.5.dp, Gray400),
                 RoundedCornerShape(16.dp)
-            ),
+            )
+            .fillMaxWidth(),
     ) {
         Row(
             modifier = Modifier.padding(8.dp),
